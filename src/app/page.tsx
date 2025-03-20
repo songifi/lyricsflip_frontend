e3538f855c0646c5b853b3f2cc6ba057addc0779
Kaylahray <chiomachristopher91@gmail.com>
2025-03-21 00:26:59 +0100

    initialized a NextJs project.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className=" text-2xl md:text-3xl">Welcome to LyricsFlip 🎵😁</h1>
+    </main>
+  );
+};
+
+export default Home;
